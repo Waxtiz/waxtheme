bce893dceeeff5e59bde49d041fb620e79b5b069
t_leclerc <t_leclerc@hotmail.fr>
2022-10-10 10:45:31 +0200

    Add palette colors and scales function

---
+++ R/scales_wax.R
@@ -0,0 +1,115 @@
+#' Wax scales
+#' Personal color palette for ggplot2
+#'
+#' @examples
+#' ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+#' geom_point(size = 4) -> p
+#'
+#' p + scale_color_wax_d()
+#'
+#' @import ggplot2
+#' @import scales
+#'
+#' @export
+
+wax_colours <- list(
+  d_pal = c(
+            "#45c7c8",
+            "#d04e2c",
+            "#626edd",
+            "#65c653",
+            "#af5dd3",
+            "#579b2f",
+            "#cc4dac",
+            "#45c27f",
+            "#e14b8c",
+            "#448237",
+            "#8451a5",
+            "#abb639",
+            "#6274b9",
+            "#dd862f",
+            "#5fa3da",
+            "#cca43f",
+            "#d08dcf",
+            "#797623",
+            "#ba3560",
+            "#6bbb8e",
+            "#d4424f",
+            "#31947c",
+            "#e58192",
+            "#31784c",
+            "#9a4d78",
+            "#a0b56c",
+            "#ac554f",
+            "#6d733b",
+            "#e0976e",
+            "#9a612b"
+  ),
+  c_binar_pal = c(
+             "#865eac",
+             "#9875b8",
+             "#aa8dc3",
+             "#bca5ce",
+             "#cebeda",
+             "#dfd7e5",
+             "#f1f1f1",
+             "#d9eadd",
+             "#c2e4cb",
+             "#a9ddb8",
+             "#90d7a5",
+             "#75cf93",
+             "#56c881"
+  ),
+  c_single_hue = c(
+             "#003f5c",
+             "#284a78",
+             "#58508d",
+             "#8b5196",
+             "#bc5090",
+             "#e4537e",
+             "#ff6361"
+  )
+)
+
+
+wax_palettes <- function(name, n, all_palettes = wax_colours, type = c("discrete", "continuous")) {
+  palette <- all_palettes[[name]]
+  if (missing(n)) {
+    n <- length(palette)
+  }
+  type <- match.arg(type)
+  out <- switch(type,
+               continuous = grDevices::colorRampPalette(palette)(n),
+               discrete = palette[1:n]
+  )
+  structure(out, name = name, class = "palette")
+}
+
+
+scale_colour_wax_d <- function(name = "d_pal") {
+  ggplot2::scale_colour_manual(values = wax_palettes(name,
+                                                     type = "discrete"))
+}
+
+scale_fill_wax_d <- function(name = "d_pal") {
+  ggplot2::scale_fill_manual(values = wax_palettes(name,
+                                                   type = "discrete"))
+}
+
+scale_colour_wax_c <- function(name = "c_single_hue") {
+  ggplot2::scale_colour_gradientn(colours = wax_palettes(name = name,
+                                                         type = "continuous"))
+}
+
+scale_fill_wax_c <- function(name = "c_single_hue") {
+  ggplot2::scale_fill_gradientn(colours = wax_palettes(name = name,
+                                                       type = "continuous"))
+}
+
+scale_color_wax_d <- function() {
+  scale_colour_wax_d()
+}
+
+scale_color_wax_c <- function() {
+  scale_colour_wax_c()
+}
--- R/theme_test.R
+++ R/theme_test.R
@@ -1,34 +1,27 @@
-library(ggplot2)
-library(showtext)
+#' Wax theme
+#' Personal theme and typography for ggplot2
+#'
+#' @examples
+#' ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+#' geom_point(size = 4) -> p
+#'
+#' p + theme_wax()
+#'
+#' @import ggplot2
+#' @import showtext
+#'
+#' @export
+
 font_add_google(name = "Fira Sans", family = "fira-sans")
 font_add_google(name = "Roboto Slab", family = "roboto-slab")
 showtext_auto()
 
-
-df <- iris
-df2 <- diamonds
-
-ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
-  geom_point()+
-  labs(title = "Lorem ipsum abel ador test",
-       subtitle = "la meme chose car je ne suis vraiement pas inspire",
-        caption = "ici n'ont plus je ne sais pas quoi dire, donc je radote") -> p
-
-ggplot(df2, aes(cut, carat, color = color)) +
-  geom_boxplot()+
-  labs(title = "Lorem ipsum abel ador test",
-       subtitle = "la meme chose car je ne suis vraiement pas inspire",
-        caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et urna sit amet diam tincidunt
-                   tincidunt vel a ante. Donec accumsan metus turpis, ac luctus libero efficitur quis.
-                   Pellentesque id odio in dui rutrum vulputate id nec diam. Quisque porta in erat sed maximus.") -> p2
-
-
-theme_test <- function(base_size = 13,
-                       family_title = "roboto-slab",
-                       family_text = "fira-sans",
-                       color_title = "#33302C",
-                       color_text = "#1A2024",
-                       color_grid = "#E8E3DC") {
+theme_wax <- function(base_size = 13,
+                      family_title = "roboto-slab",
+                      family_text = "fira-sans",
+                      color_title = "#33302C",
+                      color_text = "#1A2024",
+                      color_grid = "#E8E3DC") {
 
   theme_light(base_size = base_size) %+replace%
     theme(
@@ -58,12 +51,10 @@ theme_test <- function(base_size = 13,
       axis.title.y = element_text(margin = margin(0, 0.5, 0, 0, unit = "cm"), angle =90),
       legend.position = "bottom",
       legend.box = "horizontal",
+      panel.spacing.x = grid::unit(2, "lines"), # Space between facets
       # Other
       legend.key.size = unit(.5, 'cm'),
       axis.ticks = element_blank(),
       strip.text = element_text(size = rel(1), face = "bold", margin = margin(0, 0, .25, 0, unit = "cm"))
     )
 }
-
-p2 + facet_wrap(~color) + theme_test()
-p + theme_test()
